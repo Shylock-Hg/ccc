120628c79a2bfef9292608a8e5b788b5e3373d29
Shylock-Hg <tcath2s@gmail.com>
2019-06-04 21:42:57 +0800

    Generate gcda files by workaround.

--- .ci/test-bazel.sh
+++ .ci/test-bazel.sh
@@ -9,3 +9,16 @@ readonly tests=(
 )
 
 bazel coverage --collect_code_coverage "${tests[@]}"
+
+# workaround to generate gcda files
+readonly tests_=(
+    "tests/argc_test"
+    "tests/clog_test"
+    "tests/sdb_test"
+    "tests/bits_test"
+)
+
+for t in $tests_
+do
+    ./bazel-bin/$t
+done
